2a0b0d49de009ea98bc6f5cb1387149c48d71c6e
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2020-12-21 15:11:18 +0800

    Add files via upload

---
+++ UserDao.java
@@ -0,0 +1,15 @@
+package dao;
+
+import java.util.List;
+
+import entity.User;
+
+/**
+ * 映射器:该接口中的方法对应的方法名时映射文件中那个唯一的id
+ * @author HP
+ *
+ */
+public interface UserDao {
+	//查询所有用户数据
+	//List<User> findAllUser();
+}
