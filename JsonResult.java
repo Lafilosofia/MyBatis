407982a06be9b1f7cfc2bc5233c139bb3f8b81d4
Lihaozhe <55686617+Lafilosofia@users.noreply.github.com>
2020-12-26 09:18:55 +0800

    Add files via upload

--- JsonResult.java
+++ JsonResult.java
@@ -1,20 +1,31 @@
 package web;
 
-public class JsonResult {
-	
+public class JsonResult<T> {
+	private T data;//正确数据
 	private int state;//状态(成功与否)
 	private String message;//正确/错误返回的提示信息
 	//声明两种状态
 	public static final int SUCCESS = 1;//成功的状态
 	public static final int ERROR = 0;//错误的状态
 
-	
+	public JsonResult(T data) {
+		this.data = data;
+		this.state = SUCCESS;
+		this.message = "OK";
+	}
 	
 	public JsonResult(Throwable e){
 		this.state = ERROR;
 		this.message = e.getMessage();
 	}
 
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
 
 	public int getState() {
 		return state;
@@ -32,18 +43,14 @@ public class JsonResult {
 		this.message = message;
 	}
 
-
 	@Override
 	public String toString() {
-		return "JsonResult [state=" + state + ", message=" + message + "]";
+		return "JsonResult [data=" + data + ", state=" + state + ", message=" + message + "]";
 	}
 
 
-	public JsonResult() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
+	
+	
 	
 	
 }
